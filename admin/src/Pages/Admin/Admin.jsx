38d90757d99e6483e900625514086d2b20ccf594
srinivasupenke <srinudps123@gmail.com>
2025-01-02 16:02:35 +0530

    E-Commerse

--- admin/src/Pages/Admin/Admin.jsx
+++ admin/src/Pages/Admin/Admin.jsx
@@ -3,8 +3,8 @@ import "./Admin.css";
 import Sidebar from "../../Components/Sidebar/Sidebar";
 
 import { Routes, Route } from "react-router-dom";
-import AddProduct from "../../Components/Addproduct/Addproduct";
-import ListProduct from "../../Components/ListProduct/ListProduct";
+import AddProduct from "../../Components/Addproduct/AddProduct.jsx";
+import ListProduct from "../../Components/ListProduct/ListProduct.jsx";
 const Admin = () => {
   return (
     <div className="admin">
