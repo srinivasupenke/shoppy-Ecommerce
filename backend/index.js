acf4e09e2c982f36cbd0fdf7559210ce29e520bd
srinivasu Penke <127410005+srinivasupenke@users.noreply.github.com>
2025-01-02 18:46:24 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -21,11 +21,7 @@ app.use(
 );
 
 
-app.use(express.static(path.join(__dirname, "build")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
 
 //Databse Connection with MongoDB
 
